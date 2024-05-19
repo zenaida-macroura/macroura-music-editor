d7fd6603deb3a4167a768a4d13c45c5180bc6999
Renée Waverly Sonntag <waverly@vivaldi.net>
2024-05-19 17:27:12 -0500

    Added Chord Sequencer

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # macroura-music-editor
 An MML &amp; MIDI editor targeting MuSICA for the MSX.
+
+## Documentation
+
+### Sketch Tools
+
+#### Chord Sequencer
+
+##### Chord Label Notation
+
+The chord sequencer makes the base assumption that every chord is a tetrad built from a scale.  The 1 is implied by specifying the scale from which to build the chord, and all other notes in the tetrad must be specified.
+
+The scale is determined by the initial letter in the tetrad.  Originally the chromatic scale was mapped from o-z to save characters, but we intend to also allow specification by sharps and flats. (TODO)
+
+```
+New Schema: c  c# d  d# e  f  f# g  g# a  a# b (WIP)
+Old Schema: o  p  q  r  s  t  u  v  w  x  y  z
+
+```
+After the first character defines the scale, the remaining characters specify which degrees in the scale build the tetrad (after the 1).  Some important distinctions here is that `7` refers to the major seven, so for a normal seventh chord, which uses the flat seven, use `s` instead.  Following that schema, the flat nine is `n`, the flat ten is `t`, the flat twelve is `w`, and the flat thirteen is `r`.  Another distinction of note is that flat and sharp five is indicated by `-` and `+`.
+```
+1 2 34 5 6 78 9 ab c d
+ u m  - + s  n t  w r
+```
+When you wish to have a triad instead of a tetrad, replace one of the notes in the scale with `0` and a rest will be played for that channel.
+
+Examples:
+* C major triad: `o350`
+* Cm7b5: `om-s`
+* Em9: `sms9` (drop the five)
+* F#7: `u35s`
+* F#M7: `u357`
+
+##### Sequence of Chords
+
+Example:
+```
+cdsn=[o350]2r2[xm50]2r2[t350]2r2[v350]2r2
+```
+Is converted to:
+```
+cds1=
+cds2=
+cds3=
+cds4=
+```
---
+++ src/chord-sequencer.js
@@ -0,0 +1,65 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// Let's start with the constants
+const chromatic_scale =   ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
+const scale_identifiers = ['o', 'p',  'q', 'r',  's', 't', 'u',  'v', 'w',  'x', 'y',  'z'];
+const sequence_degrees =  ['1', 'u',  '2', 'm',  '3', '4', '-',  '5', '+',  '6', 's',  '7',
+                           '8', 'n',  '9', 't',  'a', 'b', 'w',  'c', 'r',  'd'];
+const octave = 12;
+
+// Take in a label string (of any length) and convert it into an array of the notes making up the chord.
+// Future proofing against our current four note / tetrad limitation.
+function convertLabelToNoteArray(label) {
+	// First, shift the chromatic scale so that it starts with the right note.
+	let scale_offset = scale_identifiers.indexOf(label[0]);
+	if (scale_offset === -1) {
+		console.error('Start of chord label invalid for ' + label);
+		return [];
+	}
+	// Then, iterate through the specified scale degrees, converting them to notes.
+	let notes = [chromatic_scale[(scale_offset)%octave]];
+	for (let i = 1; i < label.length; i++) {
+		let note_offset = sequence_degrees.indexOf(label[i]);
+		if (label[i] === '0') {
+			notes.push('r');
+			continue;
+		}
+		if (note_offset === -1) {
+			console.warn('Note specification at position ' + i + ' in chord ' + label + ' is invalid.');
+			continue;
+		}
+		notes.push(chromatic_scale[(scale_offset + note_offset)%octave]);
+	}
+	return notes;
+}
+
+// Process a block definition which contains chord labels
+function convertBlockToChords(block_definition) {
+	// Split into an array on chord label start
+	let chords = block_definition.split('[');
+	if (chords.length === 1) {
+		// no chords
+		return [block_definition];
+	}
+	// Get the block label, split it into four labels
+	let prelude = chords[0].split('=');
+	let blocks = [];
+	for (let b = 1; b <= 4; b++) {
+		blocks.push(prelude[0].substr(0,3) + b + '=' + prelude[1]);
+	}
+	// Iterate through the chords
+	for (let c = 1; c < chords.length; c++) {
+		// Separate the chord label from anything following it; get the notes from the label
+		let before_and_after = chords[c].split(']');
+		let notes = convertLabelToNoteArray(before_and_after[0]);
+		// for each note, add it to the corresponding output block, then the things following
+		for (let n = 0; n < 4/*notes.length*/; n++) {
+			blocks[n] += notes[n] + before_and_after[1];
+		}
+	}
+	return blocks;
+}
